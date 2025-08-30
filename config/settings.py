856aff9beb29f8e706122cfcf07e2c63b3aeddcb
Jaroslav-S <sajja@post.cz>
2025-08-30 12:01:23 +0200

    settings.py updated.

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
+    'rest_framework',
+    'django_filters',
+
+    # Local apps
     'instruments',
 ]
 
