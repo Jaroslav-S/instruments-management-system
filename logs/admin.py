fba523fe8f941533136468b78dcc008e41405365
Jaroslav-S <sajja@post.cz>
2025-09-12 16:18:42 +0200

    logs/admin.py registration of LogEntry model with list_display, search_fields and list_filter.

--- logs/admin.py
+++ logs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import LogEntry
 
-# Register your models here.
+# -----------------------------
+# LogEntry model admin
+# -----------------------------
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    # Columns displayed in the list view
+    list_display = ('timestamp', 'user', 'action', 'inventory_item', 'note')
+    # Fields that can be searched using the search box
+    search_fields = ('user__username', 'action', 'note', 'inventory_item__item')
+    # Filters available in the right sidebar
+    list_filter = ('action', 'timestamp')
