34b97a14c5bfeafa921fc2d84108c6a9d785a696
Jaroslav-S <sajja@post.cz>
2025-09-13 16:11:25 +0200

    Add /login/ redirect to /frontend/login/

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from frontend import views as frontend_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +13,7 @@ urlpatterns = [
     path("frontend/menu/", frontend_views.menu_page, name="menu"),
     path("frontend/data-entry/", frontend_views.data_entry_page, name="data_entry"),
     path("frontend/view-data/", frontend_views.view_data_page, name="view_data"),
+    path('login/', RedirectView.as_view(url='/frontend/login/', permanent=False)),
 ]
 
 
