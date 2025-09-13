e9563888cfcb22d79b295d1067a94271a2971b66
Jaroslav-S <sajja@post.cz>
2025-09-13 10:53:52 +0200

    feat(api): add DRF viewsets for instruments models.

--- instruments/api/views.py
+++ instruments/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from instruments.models import Inventory, Purchases, Servicing, Rentals, Disposals
+from instruments.api.serializers import InventorySerializer, PurchasesSerializer, ServicingSerializer, RentalsSerializer, DisposalsSerializer
 
-# Create your views here.
+class InventoryViewSet(viewsets.ModelViewSet):
+    queryset = Inventory.objects.all()
+    serializer_class = InventorySerializer
+
+class PurchasesViewSet(viewsets.ModelViewSet):
+    queryset = Purchases.objects.all()
+    serializer_class = PurchasesSerializer
+
+class ServicingViewSet(viewsets.ModelViewSet):
+    queryset = Servicing.objects.all()
+    serializer_class = ServicingSerializer
+
+class RentalsViewSet(viewsets.ModelViewSet):
+    queryset = Rentals.objects.all()
+    serializer_class = RentalsSerializer
+
+class DisposalsViewSet(viewsets.ModelViewSet):
+    queryset = Disposals.objects.all()
+    serializer_class = DisposalsSerializer
